cb6b3f04dcfc03a1f8e46f301e40049b11f73814
Aurelio De Rosa <aurelioderosa@gmail.com>
2015-11-22 15:39:20 +0000

    Fixed auth tests

--- test/test.auth.js
+++ test/test.auth.js
@@ -36,8 +36,8 @@ describe('Github constructor (failing case)', function() {
 
    it('should fail authentication and return err', function(done) {
       user.notifications(function(err) {
-         err.request.status.should.equal(401, 'Return 401 status for bad auth');
-         err.request.data.message.should.equal('Bad credentials');
+         err.error.should.equal(401, 'Return 401 status for bad auth');
+         err.request.message.should.equal('Bad credentials');
          done();
       });
    });
